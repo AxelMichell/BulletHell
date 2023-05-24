7ba13aac44363c885ad1296dfaf86a9d32938083
AxelMichell <106756714+AxelMichell@users.noreply.github.com>
2023-05-24 10:53:57 -0600

    BulletSystem

--- Test/Assets/Scripts/Bullet/RadialBulletController.cs
+++ Test/Assets/Scripts/Bullet/RadialBulletController.cs
@@ -1,21 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class RadialBulletController : MonoBehaviour
 {
     [SerializeField]
-    private int bulletsAmount = 10;        
-
+    private int bulletsAmount = 10;
     private Vector3 startPoint;
+    private bool firstBalls;
+    private bool secondBalls;
+    private bool thirdBalls;
+    public bool canInvoke;
+    public float timerPerInvoke;
+    public float timer;
     [SerializeField]
     private float starAngle = 90f, endAngle = 270f;
     private const float radius = 1F;
-    private Vector3 bulletMoveDirection;
 
     private void Start()
     {
-        InvokeRepeating("SpawnProjectile", 2f, 2f);
+        firstBalls = true;
+        secondBalls = false;
+        thirdBalls = false;
+
+        canInvoke = true;
+
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timerPerInvoke += Time.deltaTime;
+
+        if(timerPerInvoke >= 2f)
+        {
+            CanInvoke();
+            timer = 0;
+            timerPerInvoke = 0;
+        }
+
+        if (canInvoke)
+        {
+
+            timer += Time.deltaTime;
+
+            if (firstBalls && timer >= 0.5f)
+            {
+                Invoke("SpawnProjectile180", 0f);
+                timer = 0f;
+            }
+
+            if (secondBalls && timer >= 0.5f)
+            {
+                Invoke("SpawnProjectile170", 0f);
+                timer = 0f;
+            }
+
+            if (thirdBalls && timer >= 0.5f)
+            {
+                Invoke("SpawnProjectile180Again", 0f);
+                timer = 0f;
+            }
+        }
     }
 
 
@@ -37,7 +84,7 @@ public class RadialBulletController : MonoBehaviour
     }
 
 
-    private void SpawnProjectile()
+    private void SpawnProjectile180()
     {
         float angleStep = (endAngle - starAngle) / bulletsAmount;
         float angle = starAngle;
@@ -55,13 +102,84 @@ public class RadialBulletController : MonoBehaviour
             GameObject bul = BulletPool.instance.GetBullet();
             bul.transform.position = transform.position;
             bul.transform.rotation = transform.rotation;
-                ;
+            ;
             bul.SetActive(true);
             bul.GetComponent<Bullet>().SetMoveDirection(projectileMoveDirection);
-    
-    
+
+
 
             angle += angleStep;
         }
+
+        firstBalls = false;
+        secondBalls = true;
     }
+
+    private void SpawnProjectile170()
+    {
+        float angleStep = (endAngle - starAngle) / bulletsAmount;
+        float angle = starAngle;
+
+        for (int i = 0; i < bulletsAmount + 1; i++)
+        {
+            float projectileDirXPosition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 170) * radius;
+            float projectileDirYPosition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 170) * radius;
+
+
+            Vector3 projectileVector = new Vector3(projectileDirXPosition, projectileDirYPosition, 0);
+            Vector3 projectileMoveDirection = Normalizar(projectileVector - startPoint);
+
+
+            GameObject bul = BulletPool.instance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            ;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(projectileMoveDirection);
+
+
+
+            angle += angleStep;
+        }
+
+        secondBalls = false;
+        thirdBalls = true;
+    }
+
+    private void SpawnProjectile180Again()
+    {
+        float angleStep = (endAngle - starAngle) / bulletsAmount;
+        float angle = starAngle;
+
+        for (int i = 0; i < bulletsAmount + 1; i++)
+        {
+            float projectileDirXPosition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirYPosition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+
+            Vector3 projectileVector = new Vector3(projectileDirXPosition, projectileDirYPosition, 0);
+            Vector3 projectileMoveDirection = Normalizar(projectileVector - startPoint);
+
+
+            GameObject bul = BulletPool.instance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            ;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(projectileMoveDirection);
+
+
+
+            angle += angleStep;
+        }
+
+        thirdBalls = false;
+        firstBalls = true;
+    }
+
+    private void CanInvoke()
+    {
+        canInvoke = !canInvoke;
+    }
+
 }
